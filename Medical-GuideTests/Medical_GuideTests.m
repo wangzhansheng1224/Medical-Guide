ca56b9c197a175d640df02c4329f3edaee534e7e
wangzs <wangzs@smartdot.com>
2017-09-12 16:55:09 +0800

    初版

---
+++ Medical-GuideTests/Medical_GuideTests.m
@@ -0,0 +1,39 @@
+//
+//  Medical_GuideTests.m
+//  Medical-GuideTests
+//
+//  Created by 王战胜 on 2017/9/12.
+//  Copyright © 2017年 gocomtech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Medical_GuideTests : XCTestCase
+
+@end
+
+@implementation Medical_GuideTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
